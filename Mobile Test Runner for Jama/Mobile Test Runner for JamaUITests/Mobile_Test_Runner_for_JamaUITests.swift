112e434e18f12e07540ba534e715c40ba312f463
David <dwagg@pdx.edu>
2017-06-27 21:29:33 -0700

    Created xcode project

---
+++ Mobile Test Runner for Jama/Mobile Test Runner for JamaUITests/Mobile_Test_Runner_for_JamaUITests.swift
@@ -0,0 +1,36 @@
+//
+//  Mobile_Test_Runner_for_JamaUITests.swift
+//  Mobile Test Runner for JamaUITests
+//
+//  Created by PSU2 on 6/27/17.
+//  Copyright © 2017 Jaca. All rights reserved.
+//
+
+import XCTest
+
+class Mobile_Test_Runner_for_JamaUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
