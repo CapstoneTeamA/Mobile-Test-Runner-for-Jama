f90789b38f03b0e5b1cd77f20403aadd1bf190f2
jason Ritz <jritz@pdx.edu>
2017-07-05 14:26:50 -0700

    created log in screen in main storyboard and added code to make the keyboard dissapear when the user clicks outside of the keyboard to the loginViewControler

---
+++ Mobile Test Runner for Jama/Mobile Test Runner for Jama/MaxTextLength.swift
@@ -0,0 +1,38 @@
+//
+//  MaxTextLength.swift
+//  Mobile Test Runner for Jama
+//
+//  Created by PSU1 on 7/5/17.
+//  Copyright © 2017 Jaca. All rights reserved.
+//
+
+import Foundation
+import UIKit
+private var __maxLengths = [UITextField: Int]()
+extension UITextField {
+    @IBInspectable var maxLength: Int {
+        get {
+            guard let l = __maxLengths[self] else {
+                return 150 // (global default-limit. or just, Int.max)
+            }
+            return l
+        }
+        set {
+            __maxLengths[self] = newValue
+            addTarget(self, action: #selector(fix), for: .editingChanged)
+        }
+    }
+    func fix(textField: UITextField) {
+        let t = textField.text
+        textField.text = t?.safelyLimitedTo(length: maxLength)
+    }
+}
+
+extension String
+{
+    func safelyLimitedTo(length n: Int)->String {
+        let c = self.characters
+        if (c.count <= n) { return self }
+        return String( Array(c).prefix(upTo: n) )
+    }
+}
