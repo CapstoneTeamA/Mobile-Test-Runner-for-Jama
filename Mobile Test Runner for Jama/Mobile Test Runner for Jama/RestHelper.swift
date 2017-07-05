e7e03ae7b2683efeb01293cba196b04ad74bc439
David <dwagg@pdx.edu>
2017-07-05 16:34:59 -0700

    Added a method that can call a rest endpoint.

--- Mobile Test Runner for Jama/Mobile Test Runner for Jama/RestHelper.swift
+++ Mobile Test Runner for Jama/Mobile Test Runner for Jama/RestHelper.swift
@@ -7,3 +7,77 @@
 //
 
 import Foundation
+
+protocol EndpointDelegate {
+    func didLoadEndpoint(data: [[String: AnyObject]]?)
+}
+
+class RestHelper {
+    
+    static func basicAuth(url: URL, username: String, password: String) -> URLRequest {
+        var request = URLRequest(url: url)
+        let loginString = username + ":" + password
+        let loginData = loginString.data(using: String.Encoding.utf8)
+        guard let base64LoginString = loginData?.base64EncodedString() else {
+            print("Could not encode user info for test")
+            return URLRequest(url: url)
+        }
+        
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("Basic \(base64LoginString)", forHTTPHeaderField: "Authorization")
+        return request
+    }
+    
+    static func hitEndpoint(atEndpointString: String, withDelegate: EndpointDelegate, httpMethod : String = "Get", username: String, password: String) {
+        //Create a url from the endpoint string
+        guard let url = URL(string: atEndpointString) else {
+            print("Bad string for url")
+            return
+        }
+        
+        //Get a URLRequest with basic auth
+        var request = RestHelper.basicAuth(url: url, username: username, password: password)
+        request.httpMethod = httpMethod
+        let session = URLSession.shared
+        
+        let task = session.dataTask(with: request) {
+            (data,response,error) in
+            guard error == nil else {
+                print("error calling endpoint")
+                print(error as Any)
+                return
+            }
+            guard let responseData = data else {
+                print("Error did not recieve data")
+                return
+            }
+            var endpointData : [[String: AnyObject]]  = []
+            //Parsing json
+            do {
+                guard let jsonData = try JSONSerialization.jsonObject(with: responseData, options: [])
+                    as? [String: Any] else {
+                        print("error trying to convert to JSON")
+                        return
+                }
+                //Get the meta section of the response to get the status
+                var meta: [String:AnyObject] = jsonData["meta"] as! Dictionary
+                let status = meta["status"] as! String
+                
+                //If user isn't authorized show an auth failed message
+                if (status == "Unauthorized") {
+                    endpointData = []
+                    
+                } else {
+                    //user authorized, parse data section of response and print greeting
+                    endpointData = jsonData["data"] as! Array
+                }
+                withDelegate.didLoadEndpoint(data: endpointData)
+                
+            } catch {
+                print("error trying to convert to json")
+            }
+        }
+        task.resume()
+    }
+}
--- Mobile Test Runner for Jama/Mobile Test Runner for JamaTests/RestHelperUnitTests.swift
+++ Mobile Test Runner for Jama/Mobile Test Runner for JamaTests/RestHelperUnitTests.swift
@@ -7,8 +7,13 @@
 //
 
 import XCTest
+@testable import Mobile_Test_Runner_for_Jama
 
 class RestHelperUnitTests: XCTestCase {
+    let username = "tester"
+    let password = "password"
+    var restData : [[String: AnyObject]] = []
+    let encodedUserInfo = "dGVzdGVyOnBhc3N3b3Jk"
     
     override func setUp() {
         super.setUp()
@@ -32,4 +37,39 @@ class RestHelperUnitTests: XCTestCase {
         }
     }
     
+    func testBasicAuthRequest() {
+        guard let url = URL(string: "pathToResource.jaca.com/rest/test") else {
+            print("URL was not constructed")
+            return
+        }
+        
+        var request = URLRequest(url: url)
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("Basic " + encodedUserInfo, forHTTPHeaderField: "Authorization")
+        let returnedRequest = RestHelper.basicAuth(url: url, username: username, password: password)
+        
+        XCTAssertEqual(request, returnedRequest)
+    }
+    
+    func testBasicAuthRequestFail() {
+        guard let url = URL(string: "pathToResource.jaca.com/rest/test") else {
+            print("URL was not constructed")
+            return
+        }
+        
+        //Create an auth string that is not the 64bit encoded username:password string.
+        let badAuthString = encodedUserInfo + "SomeBadStuff"
+        
+        var request = URLRequest(url: url)
+        request.addValue("application/json", forHTTPHeaderField: "Accept")
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        request.addValue("Basic " + badAuthString, forHTTPHeaderField: "Authorization")
+        let returnedRequest = RestHelper.basicAuth(url: url, username: username, password: password)
+        
+        XCTAssertNotEqual(request, returnedRequest)
+    }
+    
+    
 }
+
