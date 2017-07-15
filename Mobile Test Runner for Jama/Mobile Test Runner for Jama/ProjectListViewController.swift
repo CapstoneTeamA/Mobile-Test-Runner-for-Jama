ea88301ed5345b9421381ec98c092f230aceb98b
David <dwagg@pdx.edu>
2017-07-15 15:32:10 -0700

    Extracted some code to be testable and wrote tests for it.

--- Mobile Test Runner for Jama/Mobile Test Runner for Jama/ProjectListViewController.swift
+++ Mobile Test Runner for Jama/Mobile Test Runner for Jama/ProjectListViewController.swift
@@ -23,17 +23,22 @@ class ProjectListViewController: UIViewController {
         endpointString = "https://" + instance + "." + endpointString
         RestHelper.hitEndpoint(atEndpointString: endpointString, withDelegate: self, username: username, password: password)
         
-        let layout = UICollectionViewFlowLayout()
-        layout.itemSize = CGSize(width: UIScreen.main.bounds.width - 20, height: 60)
-        layout.minimumInteritemSpacing = 1000
-        layout.minimumLineSpacing = 6
-        
+
+        let layout = buildCollectionLayout()
         collectionView.setCollectionViewLayout(layout, animated: false)
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func buildCollectionLayout() -> UICollectionViewFlowLayout {
+        let layout = UICollectionViewFlowLayout()
+        layout.itemSize = CGSize(width: UIScreen.main.bounds.width - 20, height: 60)
+        layout.minimumInteritemSpacing = 1000
+        layout.minimumLineSpacing = 6
+        return layout
     }
 }
 
@@ -59,17 +64,18 @@ extension ProjectListViewController: UICollectionViewDelegate, UICollectionViewD
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "Cell", for: indexPath) as! ProjectCollectionViewCell
         
-        cell.projectCellLabel.text = projectList.projectList[indexPath.row].name
-        cell.layer.cornerRadius = 5.0
-        cell.layer.shadowColor = UIColor.lightGray.cgColor
-        cell.layer.shadowOffset = CGSize(width: 0, height: 2.0)
-        cell.layer.shadowRadius = 2.0
-        cell.layer.shadowOpacity = 1.0
-        cell.layer.masksToBounds = false
-        cell.layer.shadowPath = UIBezierPath(roundedRect: cell.bounds, cornerRadius: cell.contentView.layer.cornerRadius).cgPath
+        prepCell(forCell: cell, withLabelText: projectList.projectList[indexPath.row].name)
         return cell
     }
     
-    
+    func prepCell(forCell: UICollectionViewCell, withLabelText: String) {
+        forCell.layer.cornerRadius = 5.0
+        forCell.layer.shadowColor = UIColor.lightGray.cgColor
+        forCell.layer.shadowOffset = CGSize(width: 0, height: 2.0)
+        forCell.layer.shadowRadius = 2.0
+        forCell.layer.shadowOpacity = 1.0
+        forCell.layer.masksToBounds = false
+        forCell.layer.shadowPath = UIBezierPath(roundedRect: forCell.bounds, cornerRadius: forCell.contentView.layer.cornerRadius).cgPath
+    }
 
 }
--- Mobile Test Runner for Jama/Mobile Test Runner for JamaTests/ProjectListViewControllerUnitTests.swift
+++ Mobile Test Runner for Jama/Mobile Test Runner for JamaTests/ProjectListViewControllerUnitTests.swift
@@ -7,16 +7,43 @@
 //
 
 import XCTest
-
+@testable import Mobile_Test_Runner_for_Jama
 class ProjectListViewControllerUnitTests: XCTestCase {
-    
+    var viewController: ProjectListViewController!
     override func setUp() {
         super.setUp()
-        // Put setup code here. This method is called before the invocation of each test method in the class.
+        viewController = UIStoryboard(name: "Main", bundle: Bundle.main).instantiateViewController(withIdentifier: "ProjectListViewController") as! ProjectListViewController
     }
     
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
+    }
+    
+    func testBuildLayout() {
+        let layout = viewController.buildCollectionLayout()
+        let screenWidth = UIScreen.main.bounds.width
+        
+        XCTAssertEqual(6, layout.minimumLineSpacing)
+        XCTAssertEqual(1000, layout.minimumInteritemSpacing)
+        XCTAssertEqual(screenWidth - 20, layout.itemSize.width)
+        XCTAssertEqual(60, layout.itemSize.height)
+    }
+    
+    func testPrepCell() {
+        let cell = ProjectCollectionViewCell()
+        
+        viewController.prepCell(forCell: cell, withLabelText: "test")
+        
+        XCTAssertEqual(5.0, cell.layer.cornerRadius)
+        XCTAssertEqual(UIColor.lightGray.cgColor, cell.layer.shadowColor)
+        XCTAssertEqual(0, cell.layer.shadowOffset.width)
+        XCTAssertEqual(2.0, cell.layer.shadowOffset.height)
+        XCTAssertEqual(2.0, cell.layer.shadowRadius)
+        XCTAssertEqual(1.0, cell.layer.shadowOpacity)
+        XCTAssertFalse(cell.layer.masksToBounds)
+        
+        let shadowPath = UIBezierPath(roundedRect: cell.bounds, cornerRadius: cell.layer.cornerRadius).cgPath
+        XCTAssertEqual(shadowPath, cell.layer.shadowPath)
     }
 }
