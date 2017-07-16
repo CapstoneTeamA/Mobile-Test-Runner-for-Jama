0ac880d4c8d95ba70d772a1cd8ac67bdb334a1d8
jason Ritz <jritz@pdx.edu>
2017-07-16 14:23:28 -0700

    added test for success

--- Mobile Test Runner for Jama/Mobile Test Runner for JamaTests/ProjectListViewControllerUnitTests.swift
+++ Mobile Test Runner for Jama/Mobile Test Runner for JamaTests/ProjectListViewControllerUnitTests.swift
@@ -7,8 +7,11 @@
 //
 
 import XCTest
+@testable import Mobile_Test_Runner_for_Jama
 
 class ProjectListViewControllerUnitTests: XCTestCase {
+    
+    let projects: ProjectListViewController = ProjectListViewController ()
     
     override func setUp() {
         super.setUp()
@@ -18,5 +21,19 @@ class ProjectListViewControllerUnitTests: XCTestCase {
     override func tearDown() {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
         super.tearDown()
+    }
+    
+    func test(){
+        projects.currentUser.username = "demo"
+        projects.username = "demo"
+        projects.password = "password"
+        projects.instance = "capstone-sandbox"
+        
+        //For personal testing Okay to delete (jason)
+        DispatchQueue.main.asyncAfter(deadline: .now() + .seconds(3), execute: {
+            XCTAssertEqual(self.projects.projectList.projectList[0].name, "Achiever UAV Sample Set")
+            
+        })
+        
     }
 }
