1943bc10501c9a794e138901a53aeb10633a9bb1
David <dwagg@pdx.edu>
2017-07-16 02:56:20 -0700

    added effects so that when a cell is touched it changes color.

--- Mobile Test Runner for Jama/Mobile Test Runner for Jama/ProjectCollectionViewCell.swift
+++ Mobile Test Runner for Jama/Mobile Test Runner for Jama/ProjectCollectionViewCell.swift
@@ -10,4 +10,19 @@ import UIKit
 
 class ProjectCollectionViewCell: UICollectionViewCell {
     @IBOutlet weak var projectCellLabel: UILabel!
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesBegan(touches, with: event)
+        self.backgroundColor = UIColor.lightGray
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesEnded(touches, with: event)
+        self.backgroundColor = UIColor.white
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        super.touchesCancelled(touches, with: event)
+        self.backgroundColor = UIColor.white
+    }
 }
