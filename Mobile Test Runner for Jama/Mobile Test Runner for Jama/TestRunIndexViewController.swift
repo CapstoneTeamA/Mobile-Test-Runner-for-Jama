53ed50d669cbe70477dc301243f86242452240e3
David <dwagg@pdx.edu>
2017-07-29 14:50:42 -0700

    Made some changes, added in the future 'buttons' at the bottom of the step index view.

--- Mobile Test Runner for Jama/Mobile Test Runner for Jama/TestRunIndexViewController.swift
+++ Mobile Test Runner for Jama/Mobile Test Runner for Jama/TestRunIndexViewController.swift
@@ -34,17 +34,23 @@ class TestRunIndexViewController: UIViewController {
 
 extension TestRunIndexViewController: UITableViewDelegate, UITableViewDataSource {
     public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 1
+        return 20
     }
     
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell  {
         let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "TestStepCell")
         
-        cell.textLabel?.text = "       \(indexPath.row + 1)   " + runName
-        cell.textLabel?.bounds = CGRect(x: 30, y: 3, width: cell.bounds.width - 40, height: cell.bounds.height - 6)
+       // cell.textLabel?.text = "       \(indexPath.row + 1)   " + runName
+        let nameLabel = UILabel(frame: CGRect(x: cell.bounds.minX, y: cell.bounds.minY, width: cell.contentView.bounds.width - 82, height: cell.contentView.bounds.height))
+        
+        nameLabel.center = CGPoint(x: nameLabel.bounds.width/2 + 45, y: cell.contentView.center.y)
+        //I think this will need to be 2 labels so that they can all line up even when the number next to the name 
+            //is more than single digits (the name of #9 and #10 are offset
+        nameLabel.text = "\(indexPath.row + 1)     " +  runName + "aaaaaaaaaaaaaaaaaaaaaaa"
         let image = UIImage(named: "check_icon.png")
         let im = UIImageView(image: image)
         cell.contentView.addSubview(im)
+        cell.contentView.addSubview(nameLabel)
         im.center = CGPoint(x: 24, y: 22)
         
         cell.accessoryType = .disclosureIndicator
