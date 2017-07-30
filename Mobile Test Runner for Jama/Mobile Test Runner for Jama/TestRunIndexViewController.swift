c92d18bc18cfa8080f41b708d7b958c6a04dcbc5
David <dwagg@pdx.edu>
2017-07-29 23:19:35 -0700

    finished the ui design for the test step index, and fill it with dummy data for now.

--- Mobile Test Runner for Jama/Mobile Test Runner for Jama/TestRunIndexViewController.swift
+++ Mobile Test Runner for Jama/Mobile Test Runner for Jama/TestRunIndexViewController.swift
@@ -10,6 +10,7 @@ import UIKit
 
 class TestRunIndexViewController: UIViewController {
     
+    @IBOutlet weak var testRunNameLabel: UILabel!
     @IBOutlet weak var testStepTable: UITableView!
     var instance = ""
     var username = ""
@@ -19,7 +20,7 @@ class TestRunIndexViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        testRunNameLabel.text = runName
         testStepTable.reloadData()
         
     }
@@ -38,25 +39,9 @@ extension TestRunIndexViewController: UITableViewDelegate, UITableViewDataSource
     }
     
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell  {
-        let cell = UITableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "TestStepCell")
-        
-       // cell.textLabel?.text = "       \(indexPath.row + 1)   " + runName
-        let nameLabel = UILabel(frame: CGRect(x: cell.bounds.minX, y: cell.bounds.minY, width: cell.contentView.bounds.width - 82, height: cell.contentView.bounds.height))
-        
-        nameLabel.center = CGPoint(x: nameLabel.bounds.width/2 + 45, y: cell.contentView.center.y)
-        //I think this will need to be 2 labels so that they can all line up even when the number next to the name 
-            //is more than single digits (the name of #9 and #10 are offset
-        nameLabel.text = "\(indexPath.row + 1)     " +  runName + "aaaaaaaaaaaaaaaaaaaaaaa"
-        let image = UIImage(named: "check_icon.png")
-        let im = UIImageView(image: image)
-        cell.contentView.addSubview(im)
-        cell.contentView.addSubview(nameLabel)
-        im.center = CGPoint(x: 24, y: 22)
-        
-        cell.accessoryType = .disclosureIndicator
-        cell.preservesSuperviewLayoutMargins = false
-        cell.separatorInset = UIEdgeInsets.zero
-        
+        let cell = TestStepTableViewCell(style: UITableViewCellStyle.default, reuseIdentifier: "TestStepCell")
+        cell.customInit(tableWidth: tableView.bounds.width, stepNumber: indexPath.row + 1, stepName: runName)
+
         return cell
     }
 }
--- Mobile Test Runner for Jama/Mobile Test Runner for Jama/TestStepTableViewCell.swift
+++ Mobile Test Runner for Jama/Mobile Test Runner for Jama/TestStepTableViewCell.swift
@@ -9,16 +9,45 @@
 import UIKit
 
 class TestStepTableViewCell: UITableViewCell {
-
+    var statusIconImageView: UIImageView!
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
-        self.accessoryType = .disclosureIndicator
     }
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
 
         // Configure the view for the selected state
+    }
+
+    func customInit(tableWidth: CGFloat, stepNumber: Int, stepName: String) {
+        self.bounds = CGRect(x: self.bounds.minX, y: self.bounds.minY, width: UIScreen.main.bounds.width, height: self.bounds.height)
+        
+        //create an image view from the image and set its position
+        let image = UIImage(named: "check_icon.png")
+        statusIconImageView = UIImageView(image: image)
+        statusIconImageView.center = CGPoint(x: 24, y: 22)
+        
+        //Build the labels for the step number and step name
+        let numberLabel = UILabel(frame: CGRect(x: self.contentView.bounds.minX, y: self.contentView.bounds.minY, width: 40, height: self.contentView.bounds.height))
+        let nameLabel = UILabel(frame: CGRect(x: self.bounds.minX, y: self.bounds.minY, width: tableWidth - statusIconImageView.bounds.width - numberLabel.bounds.width - 50 , height: self.contentView.bounds.height))
+        
+        //set the position of the labels
+        numberLabel.center = CGPoint(x: statusIconImageView.center.x + 22 + numberLabel.bounds.width/2 , y: self.contentView.center.y)
+        nameLabel.center = CGPoint(x: numberLabel.center.x + 22 + nameLabel.bounds.width/2, y: self.contentView.center.y)
+        
+        numberLabel.text = "\(stepNumber)"
+        //added a long string at the end to show proof of concept that the name truncates correctly
+        nameLabel.text = stepName + "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+        
+        //Add all of the subviews to the content view of the cell
+        self.contentView.addSubview(statusIconImageView)
+        self.contentView.addSubview(nameLabel)
+        self.contentView.addSubview(numberLabel)
+        
+        //Set the accessory to the disclosureIndicator and make the cell dividors span the table width
+        self.accessoryType = .disclosureIndicator
+        self.preservesSuperviewLayoutMargins = false
+        self.separatorInset = UIEdgeInsets.zero
     }
 }
