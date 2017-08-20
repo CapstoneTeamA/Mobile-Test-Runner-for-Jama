28e9fa21aac71cf1a11a697557d3906de363f5b6
whuiras <whuiras@pdx.edu>
2017-08-20 14:36:25 -0700

    fixed buttons + added controller and model files

---
+++ Mobile Test Runner for Jama/Mobile Test Runner for Jama/PhotoEditModel.swift
@@ -0,0 +1,13 @@
+//
+//  PhotoEditModel.swift
+//  Mobile Test Runner for Jama
+//
+//  Created by Will Huiras on 8/20/17.
+//  Copyright © 2017 Jaca. All rights reserved.
+//
+
+import Foundation
+
+class PhotoEditModel {
+
+}
---
+++ Mobile Test Runner for Jama/Mobile Test Runner for Jama/PhotoEditViewController.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoEditViewController.swift
+//  Mobile Test Runner for Jama
+//
+//  Created by Will Huiras on 8/20/17.
+//  Copyright © 2017 Jaca. All rights reserved.
+//
+
+import UIKit
+
+class PhotoEditViewController: UIViewController, UITextFieldDelegate {
+    
+    @IBOutlet weak var SubmitPhotoButton: UIButton!
+    @IBOutlet weak var TakePhotoButton: UIButton!
+    @IBOutlet weak var RemovePhotoButton: UIButton!
+    
+}
