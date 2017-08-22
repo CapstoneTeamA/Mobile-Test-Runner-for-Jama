3c7a6990ba0f2cd991db239717fc1c73a2dde8f5
jason Ritz <jritz@pdx.edu>
2017-08-21 20:44:23 -0700

    fixed master merge issue

--- Mobile Test Runner for Jama/Mobile Test Runner for Jama/TestListViewController.swift
+++ Mobile Test Runner for Jama/Mobile Test Runner for Jama/TestListViewController.swift
@@ -197,18 +197,14 @@ extension TestListViewController: EndpointDelegate {
                         tmpList.extractRunList(fromData: unwrappedData, parentId: self.selectedTestCycleId)
                         self.totalRunsReturnedFromServer += tmpList.testRunList.count
                     
-                        //Filter the runs returned from the API to select the assignedTo value is the current user's id
-                        for run in tmpList.testRunList {
-                            if run.assignedTo == self.currentUser.id && run.testStatus == "NOT_RUN"  {
-                                self.testRunList.testRunList.append(run)
-                            }
-                        }
+
                         self.testList.reloadData()
                     
                     //Filter the runs returned from the API to select the assignedTo value is the current user's id
                     for run in tmpList.testRunList {
                         if run.assignedTo == self.currentUser.id && (run.testStatus == "NOT_RUN" || run.testStatus == "INPROGRESS") {
-                            self.testRunList.testRunList.append(run)
+                            self.testRunList.testRunList.append(run)}
+                        }
                         //Keep calling api while there are still more runs
                         if self.totalRunsReturnedFromServer < totalItems {
                             RestHelper.hitEndpoint(atEndpointString: self.buildTestRunEndpointString() + "&startAt=\(self.totalRunsReturnedFromServer)", withDelegate: self, username: self.username, password: self.password, timestamp: timestamp)
