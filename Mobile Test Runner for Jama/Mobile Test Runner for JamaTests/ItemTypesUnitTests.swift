d895cffc5756a7a7cfc3b2b27779860b992f7095
Meghan McBee <mmcbee1@pdx.edu>
2017-09-04 16:15:00 -0700

    added unit tests for item types and updated other tests that were affected by changes

---
+++ Mobile Test Runner for Jama/Mobile Test Runner for JamaTests/ItemTypesUnitTests.swift
@@ -0,0 +1,82 @@
+//
+//  ItemTypesUnitTests.swift
+//  Mobile Test Runner for Jama
+//
+//  Created by Meghan McBee on 9/4/17.
+//  Copyright © 2017 Jaca. All rights reserved.
+//
+
+import XCTest
+@testable import Mobile_Test_Runner_for_Jama
+
+class ItemTypesUnitTests: XCTestCase {
+    
+    var itemTypes: ItemTypes = ItemTypes()
+    var itemTypeData: [[String: AnyObject]] = []
+    var planitem: [String : AnyObject] = [:]
+    var cycleitem: [String : AnyObject] = [:]
+    var runitem: [String : AnyObject] = [:]
+    var wrongitem: [String : AnyObject] = [:]
+    var plankey = "TSTPL"
+    var cyclekey = "TSTCY"
+    var runkey = "TSTRN"
+    var wrongkey = "WRONG"
+    var planid = 35
+    var cycleid = 36
+    var runid = 37
+    var wrongid = 99
+    
+    let username = "demo"
+    let password = "password"
+    let instance = "capstone-sandbox"
+    
+    override func setUp() {
+        super.setUp()
+        //build API response data
+        planitem.updateValue(planid as AnyObject, forKey: "id")
+        planitem.updateValue(plankey as AnyObject, forKey: "typeKey")
+        cycleitem.updateValue(cycleid as AnyObject, forKey: "id")
+        cycleitem.updateValue(cyclekey as AnyObject, forKey: "typeKey")
+        runitem.updateValue(runid as AnyObject, forKey: "id")
+        runitem.updateValue(runkey as AnyObject, forKey: "typeKey")
+        wrongitem.updateValue(wrongid as AnyObject, forKey: "id")
+        wrongitem.updateValue(wrongkey as AnyObject, forKey: "typeKey")
+    }
+    
+    override func tearDown() {
+        super.tearDown()
+    }
+    
+    func testExtractItemsFromData() {
+        itemTypeData.append(planitem)
+        itemTypeData.append(cycleitem)
+        itemTypeData.append(runitem)
+        
+        itemTypes.extractItemTypes(fromData: itemTypeData)
+        
+        XCTAssertEqual(planid, itemTypes.planId)
+        XCTAssertEqual(plankey, itemTypes.planKey)
+        XCTAssertEqual(cycleid, itemTypes.cycleId)
+        XCTAssertEqual(cyclekey, itemTypes.cycleKey)
+        XCTAssertEqual(runid, itemTypes.runId)
+        XCTAssertEqual(runkey, itemTypes.runKey)
+    }
+    
+    func testFoundAllTypes() {
+        itemTypeData.append(planitem)
+        itemTypeData.append(cycleitem)
+        itemTypeData.append(runitem)
+        itemTypes.extractItemTypes(fromData: itemTypeData)
+        
+        XCTAssertFalse(itemTypes.didNotFindAllTypes())
+    }
+    
+    func testDidNotFindAllTypes() {
+        itemTypeData.append(planitem)
+        itemTypeData.append(cycleitem)
+        itemTypeData.append(wrongitem)
+        itemTypes.extractItemTypes(fromData: itemTypeData)
+        
+        XCTAssertTrue(itemTypes.didNotFindAllTypes())
+    }
+}
